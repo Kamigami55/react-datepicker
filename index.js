2386f90bb62595cbe56e5ee92a4702ebb43a7da9
Kamigami55 <eason@easonchang.com>
2021-03-04 14:06:28 +0800

    Reformat files with new linter config

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 class HelloMessage extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return <div>Hello {this.props.name}</div>
   }
 }
 
 const mountNode = document.getElementById('app')
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
-
+ReactDOM.render(<HelloMessage name="Jane" />, mountNode)
